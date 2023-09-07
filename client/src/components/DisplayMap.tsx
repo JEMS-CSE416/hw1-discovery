c4699a1937cb82439f8e6714f17b7f064aecf116
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2023-09-07 00:17:48 -0400

    added comments, todo, and vars to help with readibility, understanding, and abstraction of GeoJSON data

--- client/src/components/DisplayMap.tsx
+++ client/src/components/DisplayMap.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GeoJSON, MapContainer, TileLayer, Popup} from 'react-leaflet'
+import { GeoJSON, MapContainer, TileLayer, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import ReactLeafletKml from 'react-leaflet-kml'; // react-leaflet-kml must be loaded AFTER react-leaflet
 import { FileInfo } from "../utils/global_utils";
@@ -9,16 +9,16 @@ interface DisplayMapProps {
 }
 
 const DisplayMap: React.FC<DisplayMapProps> = ({ fileInfo }) => {
-    if(fileInfo.fileType == "json"){
-        if(fileInfo.fileContent){
-            //console.log(JSON.parse(fileContent)["geometries"]);
+    if (fileInfo.fileType == "json") {
+        if (fileInfo.fileContent) {
             console.log(JSON.parse(fileInfo.fileContent));
         }
     }
-    
+
     const renderMap = () => {
-        if(fileInfo.fileType === "kml"){
-            if(fileInfo.fileContent){
+        /* Render KML */
+        if (fileInfo.fileType === "kml") {
+            if (fileInfo.fileContent) {
                 const kmlText = fileInfo.fileContent;
                 const parser = new DOMParser();
                 const kml = parser.parseFromString(kmlText, 'text/xml');
@@ -26,24 +26,28 @@ const DisplayMap: React.FC<DisplayMapProps> = ({ fileInfo }) => {
                 return (
                     // TO-DO:
                     // - make center coordinates dynamic
-                    <MapContainer zoom={10} center={[39.7392, -104.9903]} style={{width:'100vw',height:'900px'}}>
+                    <MapContainer zoom={10} center={[39.7392, -104.9903]} style={{ width: '100vw', height: '900px' }}>
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                         />
                         <ReactLeafletKml kml={kml} />
                     </MapContainer>
                 )
             }
-            
-        }else{
-            if(fileInfo.fileContent){
-                let geoJSON = fileInfo.fileContent;
+
+        }
+        /* Render GeoJSON */
+        else {
+            if (fileInfo.fileContent) {
+                const geoJSON = JSON.parse(fileInfo.fileContent);
+                let features = geoJSON["features"];
                 return (
+                    // TODO: add one popup for the entire outline for the name of the country/data
                     <>
-                    <MapContainer center={[23.6978, 120.9605]} zoom={10} style={{width:'100vw',height:'900px'}}>
-                        <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
-                        <GeoJSON data={JSON.parse(geoJSON)}/>
-                    </MapContainer>
+                        <MapContainer center={[23.6978, 120.9605]} zoom={10} style={{ width: '100vw', height: '900px' }}>
+                            <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+                            <GeoJSON data={geoJSON} />
+                        </MapContainer>
                     </>
                 )
             }
